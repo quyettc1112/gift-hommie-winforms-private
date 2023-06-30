42ae78f4f28afc543099b8182aa81230d80481fc
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-06-30 20:10:07 +0700

    Set up DB + BOs

---
+++ BusinessObjects/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BusinessObjects
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTime? OrderTime { get; set; }
+        public string Message { get; set; }
+        public double? ShippingFee { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+        public DateTime? LastUpdatedTime { get; set; }
+
+        public virtual User UsernameNavigation { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ BusinessObjects/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BusinessObjects
+{
+    public partial class User
+    {
+        public User()
+        {
+            Carts = new HashSet<Cart>();
+            Orders = new HashSet<Order>();
+        }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public byte? Gender { get; set; }
+        public string Phone { get; set; }
+        public int? Yob { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+        public byte? Enabled { get; set; }
+
+        public virtual ICollection<Cart> Carts { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
